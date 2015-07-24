3375243ce3d1dfc9c9b115b5b90fa5da4c2762be
Andrew Dassonville <andrewda@users.noreply.github.com>
2015-07-23 23:24:57 -0700

    Add initial code.

--- node-trade-bot.php
+++ node-trade-bot.php
@@ -1,1 +1,27 @@
+<?php
+    $servername = ""; // mysql host
+    $username = ""; // mysql user
+    $password = ""; // mysql password
+    $dbname = ""; // mysql database
 
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    
+    $sql = "SELECT id, sid, amount FROM donations";
+    $result = $conn->query($sql);
+    
+    
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "ID: " . $row["id"]. " - SteamID: " . $row["sid"]. " - Items Donated: " . $row["amount"] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+?>
